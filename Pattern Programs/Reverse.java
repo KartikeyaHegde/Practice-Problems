06a6234509ebff0c06142c32def00af291c82797
Kartikeya Hegde <40691765+KartikeyaHegde@users.noreply.github.com>
2018-07-04 17:47:25 +0530

    Update Reverse.java

--- Pattern Programs/Reverse.java
+++ Pattern Programs/Reverse.java
@@ -1,3 +1,4 @@
+
 package org.studyeasy;
 
 public class Reverse {
@@ -14,8 +15,20 @@ public class Reverse {
 				
 				System.out.println();
 			}
+			
+			for(int i = n-1; i >= 1; i--){
+				for(int j=i; j>0; j--)
+				{
+					
+					System.out.print(j);
+				}
+				
+				System.out.println();
+			}
 							
 		}
 	 
 	}
+
+
 
